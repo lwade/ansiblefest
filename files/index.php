bc346a2e5ebf7bb08a6df1d9feb3e32fe9938e92
Lester Wade <lestertron@gmail.com>
2014-09-01 18:53:25 +0100

    Update index.php

--- files/index.php
+++ files/index.php
@@ -1,3 +1,11 @@
+<HEAD>
+<TITLE>Welcome to the httpd demo page</TITLE>
+</HEAD>
+
+<BODY BGCOLOR="WHITE">
+<CENTER>
+<H1>Test web page...</H1>
+
 <?php
 $IP = $_SERVER['REMOTE_ADDR']; // Get IP
 echo "My localmachine's IP address is: <strong>$IP</strong>";
